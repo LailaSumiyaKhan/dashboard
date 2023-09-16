ea47d5d0682cf8fb6280da0ac531de8f97556927
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-09-16 14:55:34 +0600

    Add theme and colors

---
+++ src/colors.js
@@ -0,0 +1,29 @@
+export const colors = {
+   royalBlue: '#00539C',
+   peach: '#EEA47F',
+   blue: '#2F3C7E',
+   pink: '#FBEAEB',
+   charcoal: '#101820',
+   yellow: '#FEE715',
+   coral: '#F96167',
+   limeGreen: ' #CCF381',
+   electricBlue: '#4831D4',
+   lavender: '#E2D1F9',
+   teal: '#317773',
+   cherryRed: '#990011',
+   offWhite: '#FCF6F5',
+   babyBlue: '#8AAAE5',
+   white: '#FFFFFF',
+   hotPink: '#FF69B4',
+   neonBlue: '#00FFFF',
+   burntOrange: '#EE4E34',
+   lightBlue: '#ADD8E6',
+   darkBlue: '#00008B',
+   skyBlue: '#89ABE3',
+   bubblegumPink: '#EA738D',
+   beige: '#DDC3A5',
+   tan: '#E0A96D',
+   aliceBlue: '#F0F8FF'
+}
+
+//https://webflow.com/blog/best-color-combinations
