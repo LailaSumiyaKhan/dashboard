c3b5a1ee2598d0e6bb4066020c57ba0853afd393
Laila Sumiya Khan <nilokhan207@gmail.com>
2023-09-17 15:07:12 +0600

    Add show or hide password

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,29 @@
-import { Button, Container, TextField, Typography, Box } from "@mui/material";
-import React from "react";
+import {
+  Button,
+  Container,
+  TextField,
+  Typography,
+  Box,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+  FilledInput,
+  Input,
+} from "@mui/material";
+import React, { useState } from "react";
 import { colors } from "../colors";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   return (
     <Container
       maxWidth="xs"
@@ -36,7 +57,26 @@ export default function Login() {
         </Typography>
 
         <TextField label="Username" variant="standard" />
-        <TextField label="Password" variant="standard" type="password" />
+        <FormControl variant="standard">
+          <InputLabel htmlFor="standard-adornment-password">
+            Password
+          </InputLabel>
+          <Input
+            id="standard-adornment-password"
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
         <Button
           variant="contained"
           sx={{ textTransform: "none", fontWeight: "bold", mt: 5 }}
