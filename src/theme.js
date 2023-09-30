851bedaa6ff78425cc883057fd1d4e768bd780ba
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2023-09-30 15:50:51 +0600

    Add theme

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,23 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
    typography: {
       fontFamily: 'Nunito, sans-serif',
+   },
+   palette: {
+      lightBlue: {
+         main: "#03a9f4",
+         light: "#b3e5fc",
+         dark: "#01579b"
+      },
+      blue: {
+         main: "#2196f3",
+         light: "#64b5f6",
+         dark: "#1565c0"
+      },
+      cyan: {
+         main: "#00bcd4",
+         light: "#80deea",
+         dark: "#0097a7",
+      }
    }
 });
 
